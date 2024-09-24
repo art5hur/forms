523139d8f11b81ba2cef0c05a685c89d7e423205
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-24 20:49:24 -0300

    Inicio Form

---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Form Demo';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+        ),
+        body: const MyCustomForm(),
+      ),
+    );
+  }
+}
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Define a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+String email = '';  
+final passwordController = TextEditingController();
+final emailController = TextEditingController();
+final _formKey = GlobalKey<FormState>(); 
+final  _passwordVisible = false;
+
+  @override
+  void initState() {
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: ListView(
+        padding: const EdgeInsets.all(18),
+        children: <Widget>[
+          buildEmail(),
+          const SizedBox(
+            height: 24,
+          ),
+          buildPassword(),
+          const SizedBox(
+            height: 24,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              //validar o form
+
+              //Acessar o form
+              bool valid = _formKey.currentState!.validate();
+
+              if(valid){
+                print('Email: $email');
+                print('Password: ${passwordController.text}');
+              }
+
+            },
+            child: const Text('Submit'),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildEmail() => TextFormField(
+    validator: (value) {
+      if(value == null || value.isEmpty){
+        return 'Obrigatório informar um e-mail';
+      }else{
+        // if(value ){
+
+        // }
+      }
+      return null;
+    },
+      controller: emailController,
+      onChanged: (value) {
+        email = value;
+      },
+        decoration: InputDecoration(
+          hintText: 'name@example.com',
+          labelText: 'E-mail',
+          border: const OutlineInputBorder(),
+          prefixIcon: const Icon(Icons.email),
+          suffixIcon: IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: () {
+              emailController.clear();
+            },
+          ),
+        ),
+        keyboardType: TextInputType.emailAddress,
+      );
+
+  Widget buildPassword() =>  TextFormField(
+    validator: (value) {
+      if(value == null || value.isEmpty){
+        return 'Obrigatório informar uma senha';
+      }else{
+        if(value.length < 6){
+          return 'Senha muito curta.';
+        }
+      }
+      return null;
+    },
+    controller: passwordController,
+        obscureText: !_passwordVisible,
+        decoration: InputDecoration(
+          hintText: 'Your Password...',
+          labelText: 'Password',
+          suffixIcon: IconButton(
+            icon: Icon(
+            _passwordVisible 
+            ? Icons.visibility
+            : Icons.visibility_off
+            ),
+            
+          onPressed: () {
+            setState(() {
+              _passwordVisible = !_passwordVisible;
+            });      
+          },
+          ),
+          border: OutlineInputBorder(),
+        ),
+      );
+}
